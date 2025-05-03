732bce990da8ab9f623d54930e6684313d0432ef
Juneyoung Lee <lebjuney@amazon.com>
2025-05-03 01:47:05 -0500

    ready to charge

--- arm/proofs/bignum_littleendian_4.ml
+++ arm/proofs/bignum_littleendian_4.ml
@@ -224,20 +224,3 @@ let BIGNUM_LITTLEENDIAN_4_SUBROUTINE_CORRECT = time prove
            MAYCHANGE [memory :> bignum(z,4)])`,
   ARM_ADD_RETURN_NOSTACK_TAC BIGNUM_LITTLEENDIAN_4_EXEC
     BIGNUM_LITTLEENDIAN_4_CORRECT);;
-
-(* ------------------------------------------------------------------------- *)
-(* Constant-time and memory safety proof.                                    *)
-(* ------------------------------------------------------------------------- *)
-
-needs "arm/proofs/consttime.ml";;
-needs "arm/proofs/subroutine_signatures.ml";;
-
-let full_spec = mk_safety_spec
-    (assoc "bignum_littleendian_4" subroutine_signatures)
-    BIGNUM_LITTLEENDIAN_4_SUBROUTINE_CORRECT
-    BIGNUM_LITTLEENDIAN_4_EXEC;;
-
-let BIGNUM_LITTLEENDIAN_4_SUBROUTINE_SAFE = time prove
- (full_spec,
-  PROVE_SAFETY_SPEC BIGNUM_LITTLEENDIAN_4_EXEC
-      (Array.length (snd BIGNUM_LITTLEENDIAN_4_EXEC) / 4));;
---
+++ linear-safety-specs.py
@@ -0,0 +1,29 @@
+ls = open("linear-objs.txt", "r").readlines()
+
+for l in ls:
+  l = l.split()[0]
+  obj = l[l.rfind('/')+1:]
+  fn = obj[:-len(".o")]
+
+  print(fn)
+  f = open(f"arm/proofs/{fn}.ml", "a")
+  f.write(f"""
+
+(* ------------------------------------------------------------------------- *)
+(* Constant-time and memory safety proof.                                    *)
+(* ------------------------------------------------------------------------- *)
+
+needs "arm/proofs/consttime.ml";;
+needs "arm/proofs/subroutine_signatures.ml";;
+
+let full_spec = mk_safety_spec
+    (assoc "{fn}" subroutine_signatures)
+    {fn.upper()}_SUBROUTINE_CORRECT
+    {fn.upper()}_EXEC;;
+
+let {fn.upper()}__SUBROUTINE_SAFE = time prove
+ (full_spec,
+  PROVE_SAFETY_SPEC {fn.upper()}_EXEC);;
+""")
+  f.close()
+  break
