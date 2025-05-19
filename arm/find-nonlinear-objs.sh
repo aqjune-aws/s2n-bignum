1a98056d6b64b3ccd6bb99b1af6b5e9f7bdf74b2
Juneyoung Lee <lebjuney@amazon.com>
2025-05-19 19:12:37 +0000

    Fix the straight-line/non-straight-line function counter scripts.

--- arm/find-nonlinear-objs.sh
+++ arm/find-nonlinear-objs.sh
@@ -1,7 +1,7 @@
 for i in $(find . -maxdepth 2 -name "*.o"); do
   readelf --syms ${i} >tmp.txt
-  lcount=$(cat tmp.txt | grep -v .data | grep -v .bss | grep -v .note | grep -v '\$' | grep -v .text | wc -l)
-  if [ $lcount -ne 5 ]; then
+  lcount=$(cat tmp.txt | grep -v .data | grep -v .bss | grep -v .note | grep -v '\$' | grep -v .text | grep LOCAL | wc -l)
+  if [ $lcount -ne 1 ]; then
     #~/aarch64-objdump.sh -D ${i} --section=.text >tmp.txt
     #asmcnt=$(tail +8 tmp.txt | wc -l)
     #echo $i $asmcnt
